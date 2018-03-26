518471aa73d1a079d060dabcb097a1be108df37b
Scott Werwath <swerwath@berkeley.edu>
2018-03-25 17:33:06 -0700

    Updated host ip

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def message_received(client, server, message):
 
 # Run server
 PORT=9998
-server = WebsocketServer(PORT)
+server = WebsocketServer(PORT, host="0.0.0.0")
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)

e8bea8c061b563ff39d81977b854577d6b5dbd14
Scott Werwath <swerwath@berkeley.edu>
2018-04-02 21:32:04 -0700

    proper logging

--- server.py
+++ server.py
@@ -4,13 +4,22 @@ import os
 import gc
 import datetime
 import json
+import logging
 from end2end_process import EndToEndProcessor
+
+# Set Logging
+logger = logging.getLogger('server')
+hdlr = logging.FileHandler('./server.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
 
 
 # Get last updated time
 lastm = int(os.path.getmtime("."))
 time_str = datetime.datetime.fromtimestamp(lastm).strftime('%Y-%m-%d %H:%M')
-print(time_str)
+logger.info("Starting server, last modified at " + time_str)
 
 # Load Model
 replacements = ["model/clever_replacements", "model/misc_replacements", "model/radlex_replacements"]
@@ -23,40 +32,39 @@ model_path = "model/MODEL"
 # Returns (process_report_text, ground_truth, predicted_label)
 def output_prob(text, end_to_end=e2e, state=state):
     if not state["model_loaded"]:
-        print("Loading model")
+        logger.info("Loading model")
         state["ftModel"] = ClassificationModel(path=model_path)
         state["model_loaded"] = True
     report_text = "IMPRESSION: " + text + "\nEND OF IMPRESSION"
     processed_report_text, ground_truth = e2e.transform([report_text])[0]
-    print(processed_report_text)
+    logger.info(processed_report_text)
     processed_report_text = " ".join(processed_report_text)
     prediction = state["ftModel"].predict(processed_report_text)
-    print(prediction)
-    return (processed_report_text, ground_truth, prediction)
+    logger.info(prediction)
+    logger.info (processed_report_text, ground_truth, prediction)
 
 # Server methods
 def new_client(client, server, state=state):
     state["num_clients"] += 1
     if not state["model_loaded"]:
-        print("Loading model")
+        logger.info("Loading model")
         state["ftModel"] = ClassificationModel(path=model_path)
         state["model_loaded"] = True
 
-    now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-    print(now_time + " New client connected and was given id %d" % client['id'])
+    logger.info("New client connected and was given id %d" % client['id'])
     server.send_message(client, time_str)
 
 def client_left(client, server, state=state):
     state["num_clients"] -= 1
     if state["num_clients"] == 0:
-        print("Taking model down")
+        logger.info("Taking model down")
         state["ftModel"] = None
         state["model_loaded"] = False
         gc.collect()
-        print(gc.garbage)
+        logger.info("Garbage: " + gc.garbage)
 
 def message_received(client, server, message):
-    print("Client(%d) sent: %s" % (client['id'], message))
+    logger.info("Client(%d) sent: %s" % (client['id'], message))
     obj = json.loads(message)
     if obj["type"] == "impression":
         impression = obj["payload"]
